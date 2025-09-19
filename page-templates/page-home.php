c544f01c5b06197a87833f6e8ef786d79ec0aab4
Daniel-Digid <daniel@dig.id>
2025-09-19 11:13:28 +0100

    hero

--- page-templates/page-home.php
+++ page-templates/page-home.php
@@ -8,7 +8,7 @@ if ( have_posts() ) :
 	while ( have_posts() ) :
 		the_post();
 		do_action( 'before_main_content' );
-			get_template_part( 'template-parts/pages/home/hero' );
+			get_template_part( 'template-parts/modules/section','hero' );
 			get_template_part( 'template-parts/pages/home/intro' );
 			get_template_part( 'template-parts/pages/home/four-icons' );
 			get_template_part( 'template-parts/pages/home/relaxation' );
