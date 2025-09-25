77189dc1baa6e12066c00d5a5eb774d008c4dff4
Daniel-Digid <daniel@dig.id>
2025-09-25 13:17:05 +0100

    Explore page

--- page-templates/page-explore.php
+++ page-templates/page-explore.php
@@ -9,6 +9,10 @@ if ( have_posts() ) :
 		the_post();
 		do_action( 'before_main_content' );
 			get_template_part( 'template-parts/modules/section','hero' );
+			get_template_part( 'template-parts/modules/section','intro' );
+			get_template_part( 'template-parts/pages/explore/seasons' );
+			get_template_part( 'template-parts/pages/explore/content' );
+			get_template_part( 'template-parts/pages/explore/places' );
 		do_action( 'after_main_content' );
 	endwhile;
 endif;
