f452e2bcd28d620eb01217d26ce85b788ebee68a
Daniel-Digid <daniel@dig.id>
2025-09-30 12:13:45 +0100

    impressum

---
+++ template-parts/pages/page-content.php
@@ -0,0 +1,11 @@
+<section id="section-page-content" class="section-page-content bg-lightGrey pb-12 xl:pb-28">
+	<div class="theme-container">
+		<div class="theme-grid">
+			<div class="col-span-2 md:col-span-6 xl:col-span-10">
+				<div class="section-post-content-wrapper">
+					<?php the_content(); ?>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
