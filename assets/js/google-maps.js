1a92b15ebf2ad0cd67892ada5131bb2586400da6
Daniel-Digid <daniel@dig.id>
2025-10-06 17:48:33 +0100

    Update google-maps.js

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -9,7 +9,8 @@
       // remove all POIs (icons + shapes + labels)
       { featureType: "poi", elementType: "all", stylers: [{ visibility: "off" }] },
       // (optional) also remove transit icons
-      { featureType: "transit", elementType: "all", stylers: [{ visibility: "off" }] },
+      { featureType: "poi.business", elementType: "all", stylers: [{ visibility: "off" }] }
+
       // keep your earlier tweaks
       { featureType: "administrative.land_parcel", elementType: "labels", stylers: [{ visibility: "off" }] },
       { featureType: "road.local", elementType: "labels", stylers: [{ visibility: "on" }] }
