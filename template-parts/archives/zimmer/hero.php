a1fad99ba98ea79123a2b02045ba81461443e483
Daniel-Digid <daniel@dig.id>
2025-10-08 17:39:56 +0100

    d

--- template-parts/archives/zimmer/hero.php
+++ template-parts/archives/zimmer/hero.php
@@ -11,9 +11,9 @@
       ]
     ); ?>
   <?php endif; ?>
-  <?php if ( is_front_page() ) : ?>
+  <?php /*if ( is_front_page() ) :*/ ?>
     <div class="home-hero-overlay" aria-hidden="true"></div>
-  <?php endif; ?>
+  <?php /*endif;*/ ?>
 
   <div class="theme-container justify-center h-full flex items-end pb-36 xl:pb-20 relative z-10">
     <div class="theme-grid">
