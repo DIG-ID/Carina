979ca7f70ad5861466c45ee6a2ce08e06ad438f2
Daniel-Digid <daniel@dig.id>
2025-10-08 14:11:50 +0100

    d

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,15 @@ import './navigation';
 import './sticky-header';
 import './accordion';
 import "./blog-pagination";
+
+
+/*TEMP for mailchimp form*/
+document.addEventListener('DOMContentLoaded', function() {
+  const btn = document.querySelector('.carina-newsletter .carina-btn');
+  if (!btn) return;
+
+  const lang = document.documentElement.lang || '';
+  if (lang.startsWith('fr')) btn.value = 'Envoyer';
+  else if (lang.startsWith('en')) btn.value = 'Send';
+  else btn.value = 'Senden';
+});
